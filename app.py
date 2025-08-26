2e738603db015bf6521e8af12e6cbfeed4f61a69
SYL765 <g2342k@gmail.com>
2025-08-26 13:26:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -176,4 +176,5 @@ def similar_subjects():
 
 # ✅ 서버 실행
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
